d8584608da6dc1879c6c55843d933d0b88fd8a6e
unknown <rolandaditya5@gmail.com>
2017-03-03 14:56:12 +0800

    Tugas 1 Selesai

---
+++ posttest1/src/latih2/Latih2.java
@@ -0,0 +1,16 @@
+
+package latih2;
+
+
+public class Latih2 {
+
+    
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new form1().setVisible(true);
+            }
+        });
+    }
+    
+}
